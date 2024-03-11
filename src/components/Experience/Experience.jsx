2dcfbf7a9298e52e1bfdbe56eff17d7d08ec1405
Daniel FLore <florezd43@gmail.com>
2024-03-10 22:44:44 -0500

    first Commit

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+import styles from "./Experience.module.css";
+import skills from "../../data/skills.json";
+import history from "../../data/history.json";
+import { getImageUrl } from "../../utils"
+
+export const Experience = () => {
+    return(
+       <section className={styles.container} id="experience">
+         <h2 className={styles.title}>Experience</h2>
+         <div className={styles.content}>
+            <div className={styles.skills}>
+                {skills.map((skill, id) => {
+                    return (
+                        <div key={id} className={styles.skill}>
+                          <div className={styles.skillImageContainer}>  
+                            <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                              
+                        </div>
+                        <p>{skill.title}</p>
+                        </div>
+                    )
+                })}
+
+            </div>
+            <ul className={styles.history}>
+                {history.map((historyItem, id) => {
+                    return (
+                        <li key={id} className={styles.historyItem}>
+                            <img 
+                            src={getImageUrl(historyItem.imageSrc)}
+                             alt={`${historyItem.Technology} Logo`}
+                             />
+                             <div className={styles.historyItemDetails}>
+                                <h3>{`${historyItem.role}, ${historyItem.Technology}`}</h3>
+                                <p>{`${historyItem.Duration}`}</p>
+                                {/* <ul>
+                                  {historyItem.technology.map((technology, id) => {
+                                    return <li key={id}>{technology}</li>
+                                  })}
+                                </ul> */}
+
+                             </div>
+                        </li>
+                    )
+                })}
+
+            </ul>
+
+         </div>
+
+       </section>
+    )
+}
+
